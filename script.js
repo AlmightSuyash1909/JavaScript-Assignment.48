b1fce26b10ab0d8af35942801653805584a16bcb
AlmightSuyash1909 <suyashprime1909@gmail.com>
2024-04-07 20:35:08 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// 1. List all the different error types in JavaScript with an example
+
+// let user=["oggy","Jack"]
+
+    //  (1) Syntax Error
+    // console..log(user)
+
+    //  (2) Refrence Error 
+    //  console.log(userName)
+
+    //  (3) Type error  
+    // console.log(user())
+
+    //  (4) Range Error
+    //  console.log(user.length=100*10000000000)
+
+    //  (5) Logic Error  
+    //  console.log(20+20*10/2)
+    //  console.log((20+20)*10/2)
+
+
+// 2. For the given code below write the error message and the type of error that will be thrown. Also write the reason for the error. 
+
+     //   (1) let num = if(true){
+     //           return 21;
+     //       }
+     //   -- Syntax Error
+
+     //   (2) function add(a, b){
+     //         return a + b;
+     //       }
+     //      add(21, if(){});  
+     //   -- Syntax Error   
+
+     //   (3) console.log(superHeroName);
+     //   -- Refrence Error
+
+     //   (4) let user = {
+     //         name: 'John',
+     //        };
+     //       user();
+     //   -- Type Error
+
+
+     //   (5) let num = 100;
+     //        num();
+     //    -- Type Error
+ 
+     //   (6) const arr = [1, 2, 3, 5];
+     //        arr.length = 100 * 90000000;
+     //    -- Range Error
+
+     //   (7) function main() {
+     //          console.log(adminUser);
+     //        }
+     //         main();
+     //    -- Reference Error
+
+     //   (8) let user Name = "John";
+     //       console.log(user Name);
+     //    -- Syntax Error
+
+     
+     //   (9) let age = 100;
+     //       age.toUpperCase();
+     //    -- Type Error
+
+     //   (10) let username = 'Arya Stark';
+     //        username.map(() => {});
+     //    -- Type Error
+
+
+     //   (11) function fullName(firstName, lastName) {
+     //        return firstName + ' ' + lastName;
+     //        }
+     //        let arya = fullName('Arya', lastName);
+     //    --  Reference Error
